401f36e96c7ef20aaa5e66a6a816b53a27c9fd4f
Sam Horvath <sam.horvath@kitware.com>
2025-04-24 11:28:09 -0400

    Update CTestConfig.cmake

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -1,7 +1,20 @@
-set(CTEST_PROJECT_NAME "OpenLIFU")
-set(CTEST_NIGHTLY_START_TIME "3:00:00 UTC")
+## This file should be placed in the root directory of your project.
+## Then modify the CMakeLists.txt file in the root directory of your
+## project to incorporate the testing dashboard.
+##
+## # The following are required to submit to the CDash dashboard:
+##   ENABLE_TESTING()
+##   INCLUDE(CTest)
 
-set(CTEST_DROP_METHOD "https")
-set(CTEST_DROP_SITE "open.cdash.org")
-set(CTEST_DROP_LOCATION "/submit.php?project=${CTEST_PROJECT_NAME}")
+set(CTEST_PROJECT_NAME OpenLIFU)
+set(CTEST_NIGHTLY_START_TIME 01:00:00 UTC)
+
+if(CMAKE_VERSION VERSION_GREATER 3.14)
+  set(CTEST_SUBMIT_URL https://my.cdash.org/submit.php?project=OpenLIFU)
+else()
+  set(CTEST_DROP_METHOD "https")
+  set(CTEST_DROP_SITE "my.cdash.org")
+  set(CTEST_DROP_LOCATION "/submit.php?project=OpenLIFU")
+endif()
+
 set(CTEST_DROP_SITE_CDASH TRUE)
